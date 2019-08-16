6364161318214e733d906dc3051953b1e9f124ff
Stanislav Scerbatiuc <scerbatiuc.stas@gmail.com>
2019-08-16 18:21:17 +0300

    Added Xml service that demonstrates how to input/read data to/from xml files

--- src/javaio/file/ObjectIOStreamDemo.java
+++ src/javaio/file/ObjectIOStreamDemo.java
@@ -18,21 +18,33 @@ import javaio.model.Employee;
 public class ObjectIOStreamDemo {
 
     public static void main(String[] args) {
-        Employee emp = new Employee("Ion", "Mocanu");
+        Employee[] emp = {new Employee("Ion", "Mocanu"), new Employee("Ion2", "Mocanu2")};
 
         try {
             FileOutputStream out = new FileOutputStream("C:\\Users\\sscerbatiuc.INTHER\\Desktop\\newTest.txt");
             ObjectOutputStream oout = new ObjectOutputStream(out);
 
-            oout.writeObject(emp);
+            oout.writeObject(emp[0]);
+            oout.writeObject(emp[1]);
             oout.flush();
 
+            System.out.println(Float.valueOf("0.1").floatValue());
             ObjectInputStream ois = new ObjectInputStream(new FileInputStream("C:\\Users\\sscerbatiuc.INTHER\\Desktop\\newTest.txt"));
 
             System.out.println("" + (Employee) ois.readObject());
+            System.out.println("" + (Employee) ois.readObject());
+            System.out.println(intTest(1, 2));
 
         } catch (Exception ex) {
             ex.printStackTrace();
         }
+    }
+    
+    public static int intTest(Integer a, Integer b){
+        return 1;
+    }
+    
+    public static int intTest(int a, int b){
+        return 0;
     }
 }
--- src/javaio/model/Employee.java
+++ src/javaio/model/Employee.java
@@ -6,24 +6,55 @@
 package javaio.model;
 
 import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Random;
 
 /**
  *
  * @author sscerbatiuc
  */
 public class Employee implements Serializable {
+
     String name, surname;
+    LocalDate employeedOn;
+    Integer age;
 
     public Employee(String name, String surname) {
         this.name = name;
         this.surname = surname;
     }
 
+    public Employee(String name, String surname, LocalDate employeedOn, Integer age) {
+        this(name, surname);
+        this.employeedOn = employeedOn;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public LocalDate getEmployeedOn() {
+        return employeedOn;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
     @Override
     public String toString() {
         return "Employee{" + "name=" + name + ", surname=" + surname + '}';
     }
-    
-    
-    
+
+    public static Employee getRandomEmployee() {
+        Random rnd = new Random();
+        int randValue = rnd.nextInt();
+        return new Employee("name" + randValue, "surname" + randValue, LocalDate.now(), randValue);
+    }
+
 }
